52940ba18d50dc36487e27d500b27fb5761f8c2f
Fedor Ignatov <ignatov.fedor@gmail.com>
2020-09-10 16:25:14 +0300

    feat: numbers pronunciation

--- nemo/main.py
+++ nemo/main.py
@@ -1,8 +1,10 @@
+import re
+
 import requests
+from deeppavlov import build_model, configs
 from fastapi import FastAPI, File, UploadFile
 from fastapi.responses import StreamingResponse
-
-from deeppavlov import build_model, configs
+from num2words import num2words
 
 app = FastAPI()
 asr = build_model(configs.nemo.asr)
@@ -16,6 +18,7 @@ async def create_upload_file(user_id: str, file: UploadFile = File(...)):
     post_response = requests.post('http://agent:4242', json={"user_id": user_id, "payload": transcript})
     response = post_response.json()['response']
     print(f'response is "{response}"')
+    response = re.sub(r'([0-9]+)', lambda x: num2words(x.group(0)), response)
     audio_response = tts([response])[0]
     return StreamingResponse(audio_response, media_type='audio/x-wav', headers={'response': response,
                                                                                 'transcript': transcript})
