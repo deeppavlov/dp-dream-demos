af5a991f4c379630256f6905f8cc9af5346009f7
Daniel Kornev <daniel@zetuniverse.com>
2020-11-13 16:04:10 +0300

    Update connectors.py
    removed redundant class from connectors.py

--- agent/connectors.py
+++ agent/connectors.py
@@ -12,18 +12,3 @@ logger = logging.getLogger(__name__)
 headers = {'Content-Type': 'application/json;charset=utf-8'}
 
 
-class BatchConnector:
-    def __init__(self, url: str):
-        self._url = url
-
-    async def send(self, payload: Dict, callback: Callable):
-        emotion_result = requests.request(
-            url=self._url,
-            headers=headers,
-            json=payload['payload'],
-            method='POST'
-        ).json()
-        asyncio.create_task(callback(
-            task_id=payload['task_id'],
-            response={"batch": emotion_result}
-        ))
